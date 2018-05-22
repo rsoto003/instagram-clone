5cb7e6ec79b55fd38b3185064bc9fc53f49eac0b
Ryan Soto <rsoto003@ucr.edu>
2018-05-21 18:44:44 -0700

    adding data for application

--- instagram-clone/server/server.js
+++ instagram-clone/server/server.js
@@ -33,6 +33,30 @@ let userslist = {
 }
 let postslist = {
     a: {
-        
+      a: {
+        id: "a",
+        user: userslist["a"],
+        caption: "Moving the community!",
+        image: "https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg"
+      },
+      b: {
+        id: "b",
+        user: userslist["a"],
+        caption: "Angular Book :)",
+        image:
+          "https://cdn-images-1.medium.com/max/1000/1*ltLfTw87lE-Dqt-BKNdj1A.jpeg"
+      },
+      c: {
+        id: "c",
+        user: userslist["a"],
+        caption: "Me at Frontstack.io",
+        image: "https://pbs.twimg.com/media/DNNhrp6W0AAbk7Y.jpg:large"
+      },
+      d: {
+        id: "d",
+        user: userslist["a"],
+        caption: "Moving the community!",
+        image: "https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg"
+      }
     }
-}
+  };
